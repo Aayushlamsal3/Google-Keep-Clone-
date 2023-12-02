a9a793189d631353085ebcadd02c55cef27d5c05
Aayushlamsal3 <132969642+Aayushlamsal3@users.noreply.github.com>
2023-12-02 22:33:43 +0545

    Create Header.jsx

---
+++ Header.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+const Header=()=>{
+    return(
+        <>
+        <div className="Head">
+        <img src="https://img.freepik.com/premium-vector/company-logo-business-logo-logo-brand_982550-3.jpg" height="50px" width="50px" alt="" />
+            <p>Notes</p> 
+        </div>
+        </>
+    )
+}
+
+export default Header;

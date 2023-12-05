c6cec63a3d4c3b54a5d5231586ac84a3e530649c
Aayushlamsal3 <132969642+Aayushlamsal3@users.noreply.github.com>
2023-12-05 22:50:52 +0545

    Create Foooter.jsx

---
+++ Foooter.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Foooter=()=>{
+    return(
+        <>
+       <div className="foot">
+       <p className="footer">Copyright@2023</p>
+       </div>
+        </>
+    )
+}
+
+export default Foooter;

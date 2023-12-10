59d1a64db1516d9a6e9f81753ca06db997c32908
Aayushlamsal3 <132969642+Aayushlamsal3@users.noreply.github.com>
2023-12-10 09:20:34 +0545

    Update Note.jsx

--- Note.jsx
+++ Note.jsx
@@ -3,8 +3,8 @@ import React from "react"
 const Note=(props)=>{
     return(
         <>
-   <div className="CardBox">
    <div className="cards">
+   <div className="notess">
             <p className="CardsTitle">{props.title}</p>
             <p className="CardsNotes">{props.content}</p>
         </div>
